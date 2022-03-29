450b459d9b4810f3a9505cacff9494ff45d7949c
zack <rubinzack40@gmail.com>
2022-03-29 19:15:26 -0400

    added handler for correct conf but wrong div

--- app/helpers.py
+++ app/helpers.py
@@ -16,12 +16,12 @@ class Player(BaseModel):
     
     def compare_(self, answer) -> dict:
         cmp_arr = []
-        player_attr = ["posClass", "divClass", "confClass", "ageClass", "heightClass", "numberClass", "nameClass", "teamClass"]
+        player_attr = ["posClass", "confClass", "divClass", "ageClass", "heightClass", "numberClass", "nameClass", "teamClass"]
         # go through each attribute used for comparison (Team, Div, Pos, Ht, Age, Jersey#)
 
         cmp_arr.append(bool(self.pos == answer.pos))
-        cmp_arr.append(bool(self.div == answer.div))
         cmp_arr.append(bool(self.conf == answer.conf))
+        cmp_arr.append(bool(self.div == answer.div))
         cmp_arr.append(answer.age - self.age)
         cmp_arr.append(int(answer.height) - int(self.height))
         cmp_arr.append(int(answer.number) - int(self.number))
@@ -30,24 +30,31 @@ class Player(BaseModel):
 
         res_arr = []
 
+        i = 0
         for res in cmp_arr:
             type_ = f"{type(res)}"
 
             match type_:
                 case "<class 'bool'>":
-                    if res: res_arr.append("has-background-success has-text-primary-light has-text-weight-bold")
-                    else: res_arr.append("has-background-danger-dark has-text-primary-light has-text-weight-bold")
+                    if i != 2:
+                        if res: res_arr.append("has-background-success has-text-primary-light has-text-weight-bold")
+                        else: res_arr.append("has-background-danger-dark has-text-primary-light has-text-weight-bold")
+                    else:
+                        conf_bool = cmp_arr[1]
+                        if not conf_bool: res_arr.append("has-background-danger-dark has-text-primary-light has-text-weight-bold")
+                        elif conf_bool and not res: res_arr.append("has-background-warning has-text-black-bis has-text-weight-bold")
+                        else: res_arr.append("has-background-success has-text-primary-light has-text-weight-bold")
 
                 # first 3 array elements are for table data: <td class=res_arr[:3]>, third is for arrow div: <div class=res_arr[3]>
                 case "<class 'int'>":
                     if res==0: res_arr.append("has-background-success has-text-primary-light has-text-weight-bold")
 
-                    elif res>0 and res>5: res_arr.append("has-background-danger-dark has-text-primary-light has-text-weight-bold triangle_up")
+                    elif res>0 and res>=5: res_arr.append("has-background-danger-dark has-text-primary-light has-text-weight-bold triangle_up")
                     elif res>0 and res<5: res_arr.append("has-background-warning has-text-black-bis has-text-weight-bold triangle_up")
 
                     elif res<0 and res>-5: res_arr.append("has-background-warning has-text-black-bis has-text-weight-bold triangle_down")
-                    elif res<0 and res<-5: res_arr.append("has-background-danger-dark has-text-primary-light has-text-weight-bold triangle_down")
-
+                    elif res<0 and res<=-5: res_arr.append("has-background-danger-dark has-text-primary-light has-text-weight-bold triangle_down")
+            i+=1
         res_dict = {}
 
         for i in range(8):
@@ -60,6 +67,7 @@ class Player(BaseModel):
             res_dict[k] = [v]
         print(res_dict)
         return res_dict
+
 
 def read_players() -> list:
     with open("app/players.json", "r") as f:
